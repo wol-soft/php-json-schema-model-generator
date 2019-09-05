f36b9e0f44bcc696a2375a4333afd733909b0fd5
Enno Woortmann <enno.woortmann@web.de>
2019-09-06 00:17:11 +0200

    Fix property name

--- src/PropertyProcessor/Property/AbstractPropertyProcessor.php
+++ src/PropertyProcessor/Property/AbstractPropertyProcessor.php
@@ -112,7 +112,7 @@ abstract class AbstractPropertyProcessor implements PropertyProcessorInterface
                     new PropertyCollectionProcessor(),
                     $this->schemaProcessor,
                     $this->schema,
-                    $property->getName() . '_composed_' . uniqid(),
+                    $property->getName(),
                     [
                         'type' => $composedValueKeyword,
                         'propertyData' => $propertyData,
