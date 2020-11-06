e04c3c457f904e27d3a5f7b3cc65df80e9a17f82
Enno Woortmann <enno.woortmann@web.de>
2020-11-06 11:56:06 +0100

    Add test cases for references in OpenAPI v3 spec files

--- tests/SchemaProvider/OpenAPIv3ProviderTest.php
+++ tests/SchemaProvider/OpenAPIv3ProviderTest.php
@@ -45,7 +45,7 @@ class OpenAPIv3ProviderTest extends AbstractPHPModelGeneratorTest
 
     public function testOpenApiV3SchemaProvider(): void
     {
-        $this->generateClassFromFile('MultipleSchemaDefinitions.json', null, false, true, OpenAPIv3Provider::class);
+        $this->generateClassFromFile('MultipleSchemaDefinitions.json', null, true, true, OpenAPIv3Provider::class);
 
         $person = new \OpenApiPerson(['name' => 'Hannes']);
         $this->assertSame('Hannes', $person->getName());
@@ -54,5 +54,121 @@ class OpenAPIv3ProviderTest extends AbstractPHPModelGeneratorTest
         // test if the custom ID is preferred over the object key
         $car = new \OpenApiCarWithCustomId(['ps' => 150]);
         $this->assertSame(150, $car->getPs());
+    }
+
+    /**
+     * @dataProvider referencedSchemaDataProvider
+     */
+    public function testOpenApiV3ReferencedSchemaProvider(
+        string $reference,
+        array $personData,
+        callable $personAssert,
+        array $carData,
+        callable $carAssert
+    ): void {
+        $className = $this->generateClassFromFileTemplate(
+            'References.json',
+            [$reference, $reference, $reference],
+            null,
+            false,
+            true,
+            OpenAPIv3Provider::class
+        );
+
+        $personClass = "{$className}_0";
+        $carClass = "{$className}_1";
+
+        $personAssert(new $personClass($personData));
+        $carAssert(new $carClass($carData));
+    }
+
+    public function referencedSchemaDataProvider(): array
+    {
+        return [
+            'Empty data path reference' => [
+                '#/components/modules/person',
+                [],
+                function ($person) {
+                    $this->assertNull($person->getName());
+                    $this->assertIsArray($person->getChildren());
+                    $this->assertEmpty($person->getChildren());
+                },
+                [],
+                function ($car) {
+                    $this->assertNull($car->getPs());
+                    $this->assertNull($car->getOwner());
+                },
+            ],
+            'one level data id reference' => [
+                '#Person',
+                [
+                    'name' => 'Hannes',
+                    'children' => [
+                        [
+                            'name' => 'Erwin',
+                        ],
+                    ],
+                ],
+                function ($person) {
+                    $this->assertSame('Hannes', $person->getName());
+                    $this->assertCount(1, $person->getChildren());
+                    $this->assertSame('Erwin', $person->getChildren()[0]->getName());
+                    $this->assertEmpty($person->getChildren()[0]->getChildren());
+                },
+                [
+                    'ps' => 150,
+                    'owner' => [
+                        'name' => 'Susi',
+                    ],
+                ],
+                function ($car) {
+                    $this->assertSame(150, $car->getPs());
+                    $this->assertSame('Susi', $car->getOwner()->getName());
+                    $this->assertEmpty($car->getOwner()->getChildren());
+                },
+            ],
+            'nested recursive data id reference' => [
+                '#Person',
+                [
+                    'name' => 'Hannes',
+                    'children' => [
+                        [
+                            'name' => 'Erwin',
+                            'children' => [
+                                [
+                                    'name' => 'Gerda',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                function ($person) {
+                    $this->assertSame('Hannes', $person->getName());
+                    $this->assertCount(1, $person->getChildren());
+                    $this->assertSame('Erwin', $person->getChildren()[0]->getName());
+                    $this->assertCount(1, $person->getChildren()[0]->getChildren());
+                    $this->assertSame('Gerda', $person->getChildren()[0]->getChildren()[0]->getName());
+                    $this->assertEmpty($person->getChildren()[0]->getChildren()[0]->getChildren());
+                },
+                [
+                    'ps' => 150,
+                    'owner' => [
+                        'name' => 'Susi',
+                        'children' => [
+                            [
+                                'name' => 'Gerda',
+                            ],
+                        ],
+                    ],
+                ],
+                function ($car) {
+                    $this->assertSame(150, $car->getPs());
+                    $this->assertSame('Susi', $car->getOwner()->getName());
+                    $this->assertCount(1, $car->getOwner()->getChildren());
+                    $this->assertSame('Gerda', $car->getOwner()->getChildren()[0]->getName());
+                    $this->assertEmpty($car->getOwner()->getChildren()[0]->getChildren());
+                },
+            ],
+        ];
     }
 }
