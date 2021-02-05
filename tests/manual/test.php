f9f339f331a897850c56b8fd1a4f7f4bc25798c5
Enno Woortmann <enno.woortmann@web.de>
2021-02-05 12:05:07 +0100

    revert manual test

--- tests/manual/test.php
+++ tests/manual/test.php
@@ -9,7 +9,6 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $generator = new ModelGenerator((new GeneratorConfiguration())
     ->setNamespacePrefix('\\ManualSchema')
     ->setImmutable(false)
-    ->setImplicitNull(true)
 );
 
 $generator
