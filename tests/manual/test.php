b26afa66fdbce70034df7de0463f5dfeeb21ae05
Enno Woortmann <enno.woortmann@web.de>
2021-03-26 14:05:25 +0100

    revert manual test files

--- tests/manual/test.php
+++ tests/manual/test.php
@@ -1,50 +1,16 @@
 <?php
 
-use ManualSchema\Person;
 use PHPModelGenerator\ModelGenerator;
 use PHPModelGenerator\Model\GeneratorConfiguration;
-use PHPModelGenerator\SchemaProcessor\PostProcessor\AdditionalPropertiesAccessorPostProcessor;
-use PHPModelGenerator\SchemaProcessor\PostProcessor\PatternPropertiesAccessorPostProcessor;
 use PHPModelGenerator\SchemaProvider\RecursiveDirectoryProvider;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 $generator = new ModelGenerator((new GeneratorConfiguration())
     ->setNamespacePrefix('\\ManualSchema')
-    ->setSerialization(true)
     ->setImmutable(false)
 );
 
 $generator
-    ->addPostProcessor(new AdditionalPropertiesAccessorPostProcessor(true))
-    ->addPostProcessor(new PatternPropertiesAccessorPostProcessor())
     ->generateModelDirectory(__DIR__ . '/result')
     ->generateModels(new RecursiveDirectoryProvider(__DIR__ . '/schema'), __DIR__ . '/result');
-
-$p = new Person(['name' => 'Larry', 'S_Hello' => 'Hello', 'I_World' => 'x']);
-
-print_r($p->getPatternProperties("StringProperties"));
-print_r($p->getPatternProperties("StringPropertiesShort"));
-print_r($p->getAdditionalProperties());
-
-$p->setAdditionalProperty('S_World', '          Blablabla       ');
-$p->setAge(100);
-$p->setName('Hannes');
-echo "\n############################\n\n";
-
-print_r($p->getPatternProperties('StringProperties'));
-print_r($p->getPatternProperties("StringPropertiesShort"));
-print_r($p->getAdditionalProperties());
-
-$p->removeAdditionalProperty('S_World');
-echo "\n############################\n\n";
-
-print_r($p->getPatternProperties('StringProperties'));
-print_r($p->getPatternProperties("StringPropertiesShort"));
-print_r($p->getAdditionalProperties());
-
-$p->setAdditionalProperty('nonono', 'NOOOO');
-
-print_r($p->getPatternProperties('StringProperties'));
-print_r($p->getPatternProperties("StringPropertiesShort"));
-print_r($p->getAdditionalProperties());
