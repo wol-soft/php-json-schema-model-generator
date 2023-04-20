757eb1d198bc5d927e3fc64a5f75c9fd0bb9938f
Enno Woortmann <enno.woortmann@web.de>
2023-04-20 17:50:58 +0200

    Fix issue #70
    Merged properties of a composition are filled up with the provided values without further validation execution after the single composition branches are validated separately. Consequently,changes applied to the values from filters are missing. The new added method _getModifiedValues sets up a diff with all changed values from the nested composition branch. All modified values are applied to the merged property afterwards so changes from filters are kept.

---
+++ tests/Issues/Issue/Issue70Test.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPModelGenerator\Tests\Issues\Issue;
+
+use PHPModelGenerator\Filter\TransformingFilterInterface;
+use PHPModelGenerator\Model\GeneratorConfiguration;
+use PHPModelGenerator\Tests\Issues\AbstractIssueTest;
+
+class Issue70Test extends AbstractIssueTest
+{
+    /**
+     * @dataProvider validInputDataProvider
+     */
+    public function testValidInput(string $filter, array $input, $expectedOutput): void
+    {
+        $className = $this->generateClassFromFileTemplate(
+            'filterInCompositionInArray.json',
+            [$filter],
+            (new GeneratorConfiguration())->addFilter($this->getFilter())
+        );
+
+        $object = new $className($input);
+
+        $this->assertCount(1, $object->getItems());
+        $this->assertSame('Hello', $object->getItems()[0]->getTitle());
+        $this->assertSame($expectedOutput, $object->getItems()[0]->getProperty());
+    }
+
+    public function validInputDataProvider(): array
+    {
+        return [
+            'basic filter - default value' => ['trim', ['items' => [['title' => 'Hello']]], 'now'],
+            'basic filter - custom value - not modified' => ['trim', ['items' => [['title' => 'Hello', 'property' => 'later']]], 'later'],
+            'basic filter - custom value - modified' => ['trim', ['items' => [['title' => 'Hello', 'property' => '   later   ']]], 'later'],
+            'basic filter - null' => ['trim', ['items' => [['title' => 'Hello', 'property' => null]]], null],
+            'transforming filter - default value' => ['countChars', ['items' => [['title' => 'Hello']]], 3],
+            'transforming filter - transformed value' => ['countChars', ['items' => [['title' => 'Hello', 'property' => 5]]], 5],
+            'transforming filter - custom value' => ['countChars', ['items' => [['title' => 'Hello', 'property' => 'Hello World']]], 11],
+            'transforming filter - null' => ['countChars', ['items' => [['title' => 'Hello', 'property' => null]]], null],
+        ];
+    }
+
+    public function getFilter(): TransformingFilterInterface
+    {
+        return new class () implements TransformingFilterInterface {
+            public function getAcceptedTypes(): array
+            {
+                return ['string', 'null'];
+            }
+
+            public function getToken(): string
+            {
+                return 'countChars';
+            }
+
+            public function getFilter(): array
+            {
+                return [Issue70Test::class, 'filter'];
+            }
+
+            public function getSerializer(): array
+            {
+                return [Issue70Test::class, 'filter'];
+            }
+        };
+    }
+
+    public static function filter(?string $input): ?int
+    {
+        return $input === null ? null : strlen($input);
+    }
+}
