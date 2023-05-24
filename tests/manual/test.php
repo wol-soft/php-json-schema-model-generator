781c1eb259f7d7c1986d2553beec5ef68c64c3dd
Enno Woortmann <enno.woortmann@eventim.de>
2023-05-24 15:31:28 +0200

    revert manual test

--- tests/manual/test.php
+++ tests/manual/test.php
@@ -2,20 +2,15 @@
 
 use PHPModelGenerator\ModelGenerator;
 use PHPModelGenerator\Model\GeneratorConfiguration;
-use PHPModelGenerator\SchemaProcessor\PostProcessor\EnumPostProcessor;
 use PHPModelGenerator\SchemaProvider\RecursiveDirectoryProvider;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 $generator = new ModelGenerator((new GeneratorConfiguration())
-    ->setNamespacePrefix('\\ManualSchema\\Model')
+    ->setNamespacePrefix('\\ManualSchema')
     ->setImmutable(false)
 );
 
 $generator
-    ->generateModelDirectory(__DIR__ . '/result/Model')
-    ->addPostProcessor(new EnumPostProcessor(__DIR__ . '/result/Enum', '\\ManualSchema\\Enum'))
-    ->generateModels(new RecursiveDirectoryProvider(__DIR__ . '/schema'), __DIR__ . '/result/Model');
-
-$p = new \ManualSchema\Model\Person(['name' => 'Lawrence']);
-var_export($p->getName());
+    ->generateModelDirectory(__DIR__ . '/result')
+    ->generateModels(new RecursiveDirectoryProvider(__DIR__ . '/schema'), __DIR__ . '/result');
