83acf61e02213378e068267761eb1264c452d50c
Valentin Kolodkin <kolodkinvalentin@gmail.com>
2024-09-19 13:55:45 +0300

    Added support for non required const-properties (#82)
    * Added support for non required const-properties
    
    * Replaced interface to abstract class
    
    * Added implementation of implicit null for constant properties
    
    * Changed the check setRequired
    
    * Remove modelData from check conditions
    
    * Added call generateValidators() and unit test
    
    * Replaced check with three conditions
    
    * Ensured the presence of the $modelData variable in the templates, added some unit tests
    
    ---------
    
    Co-authored-by: Kolodkin Valentin <kolodkin@playkot.com>

--- tests/Objects/ConstPropertyTest.php
+++ tests/Objects/ConstPropertyTest.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace PHPModelGenerator\Tests\Objects;
 
+use PHPModelGenerator\Exception\Arrays\InvalidTupleException;
+use PHPModelGenerator\Exception\ComposedValue\OneOfException;
+use PHPModelGenerator\Exception\ErrorRegistryException;
 use PHPModelGenerator\Exception\FileSystemException;
+use PHPModelGenerator\Exception\Object\RequiredValueException;
 use PHPModelGenerator\Exception\ValidationException;
 use PHPModelGenerator\Exception\RenderException;
 use PHPModelGenerator\Exception\SchemaException;
+use PHPModelGenerator\Model\GeneratorConfiguration;
 use PHPModelGenerator\Tests\AbstractPHPModelGeneratorTestCase;
 use stdClass;
 
@@ -38,14 +43,38 @@ class ConstPropertyTest extends AbstractPHPModelGeneratorTestCase
      * @throws RenderException
      * @throws SchemaException
      */
-    public function testNotProvidedConstPropertyThrowsAnException(): void
+    public function testProvidedArrayItemConstPropertyIsValid(): void
     {
-        $this->expectException(ValidationException::class);
-        $this->expectExceptionMessage('Invalid value for stringProperty declined by const constraint');
+        $className = $this->generateClassFromFile('ArrayItemConstProperty.json');
 
-        $className = $this->generateClassFromFile('ConstProperty.json');
+        $object = new $className(['property' => ['red', 'red']]);
 
-        new $className([]);
+        $this->assertIsArray($object->getProperty());
+        $this->assertSame(['red', 'red'], $object->getProperty());
+    }
+
+    /**
+     * @dataProvider stringIntDataProvider
+     *
+     * @throws FileSystemException
+     * @throws RenderException
+     * @throws SchemaException
+     */
+    public function testProvidedAnyOfConstPropertyIsValid(string|int $propertyValue): void
+    {
+        $className = $this->generateClassFromFile('AnyOfConstProperty.json');
+
+        $object = new $className(['property' => $propertyValue]);
+
+        $this->assertSame($propertyValue, $object->getProperty());
+    }
+
+    public function stringIntDataProvider(): array
+    {
+        return [
+            ['red'],
+            [1],
+        ];
     }
 
     /**
@@ -62,7 +91,7 @@ class ConstPropertyTest extends AbstractPHPModelGeneratorTestCase
         $this->expectException(ValidationException::class);
         $this->expectExceptionMessage('Invalid value for stringProperty declined by const constraint');
 
-        $className = $this->generateClassFromFile('ConstProperty.json');
+        $className = $this->generateClassFromFile('ConstProperty.json', null, false, false);
 
         new $className(['stringProperty' => $propertyValue]);
     }
@@ -78,5 +107,171 @@ class ConstPropertyTest extends AbstractPHPModelGeneratorTestCase
             'string' => ['null'],
             'null' => [null],
         ];
+    }
+
+    /**
+     * @throws FileSystemException
+     * @throws RenderException
+     * @throws SchemaException
+     */
+    public function testNotMatchingArrayItemConstPropertyThrowsAnException(): void
+    {
+        $this->expectException(InvalidTupleException::class);
+        $this->expectExceptionMessage('Invalid tuple item in array property');
+
+        $className = $this->generateClassFromFile('ArrayItemConstProperty.json');
+
+        new $className(['property' => ['green']]);
+    }
+
+    /**
+     * @throws FileSystemException
+     * @throws RenderException
+     * @throws SchemaException
+     */
+    public function testNotMatchingArrayItemConstPropertyThrowsAnException1(): void
+    {
+        $this->expectException(OneOfException::class);
+        $this->expectExceptionMessage('Invalid value for property declined by composition constraint');
+
+        $className = $this->generateClassFromFile('AnyOfConstProperty.json');
+
+        new $className(['property' => 'green']);
+    }
+
+    /**
+     * @throws FileSystemException
+     * @throws RenderException
+     * @throws SchemaException
+     */
+    public function testProvidedConstOnlyRequiredPropertyIsValid(): void
+    {
+        $className = $this->generateClassFromFile('RequiredAndOptionalConstProperties.json');
+
+        $object = new $className(['requiredProperty' => 'red']);
+
+        $this->assertSame('red', $object->getRequiredProperty());
+        $this->assertNull($object->getOptionalProperty());
+    }
+
+    /**
+     * @throws FileSystemException
+     * @throws RenderException
+     * @throws SchemaException
+     */
+    public function testProvidedNullOptionalPropertyConstPropertyIsValid(): void
+    {
+        $className = $this->generateClassFromFile('RequiredAndOptionalConstProperties.json');
+
+        $object = new $className(['requiredProperty' => 'red', 'optionalProperty' => null]);
+
+        $this->assertSame('red', $object->getRequiredProperty());
+        $this->assertNull($object->getOptionalProperty());
+    }
+
+    /**
+     * @dataProvider requiredAndOptionalPropertiesDataProvider
+     *
+     * @throws FileSystemException
+     * @throws RenderException
+     * @throws SchemaException
+     */
+    public function testProvidedConstPropertiesIsValidWithDifferentImplicitNull(
+        bool $implicitNull,
+        string $reqPropertyValue,
+        string $optPropertyValue
+    ): void
+    {
+        $className = $this->generateClassFromFile(
+            'RequiredAndOptionalConstProperties.json',
+            new GeneratorConfiguration(),
+            false,
+            $implicitNull,
+        );
+
+        $object = new $className(['requiredProperty' => $reqPropertyValue, 'optionalProperty' => $optPropertyValue]);
+
+        $this->assertSame($reqPropertyValue, $object->getRequiredProperty());
+        $this->assertSame($optPropertyValue, $object->getOptionalProperty());
+    }
+
+    public function requiredAndOptionalPropertiesDataProvider(): array
+    {
+        return $this->combineDataProvider(
+            $this->implicitNullDataProvider(),
+            [
+                ['red', 'green'],
+            ],
+        );
+    }
+
+    /**
+     * @throws FileSystemException
+     * @throws RenderException
+     * @throws SchemaException
+     */
+    public function testNotProvidedRequiredPropertyThrowsAnException(): void
+    {
+        $this->expectException(RequiredValueException::class);
+        $this->expectExceptionMessage('Missing required value for requiredProperty');
+
+        $className = $this->generateClassFromFile('RequiredAndOptionalConstProperties.json');
+
+        new $className([]);
+    }
+
+    /**
+     * @dataProvider invalidRequiredAndOptionalConstPropertiesDataProvider
+     *
+     * @throws FileSystemException
+     * @throws RenderException
+     * @throws SchemaException
+     */
+    public function testNotMatchingRequiredAndOptionalProvidedDataThrowsAnException(
+        bool $implicitNull,
+        string $reqPropertyValue,
+        ?string $optPropertyValue,
+        string $exceptionMessage
+    ): void
+    {
+        $className = $this->generateClassFromFile(
+            'RequiredAndOptionalConstProperties.json',
+            new GeneratorConfiguration(),
+            false,
+            $implicitNull,
+        );
+
+        $this->expectException(ErrorRegistryException::class);
+        $this->expectExceptionMessage($exceptionMessage);
+
+        new $className(['requiredProperty' => $reqPropertyValue, 'optionalProperty' => $optPropertyValue]);
+    }
+
+    public function invalidRequiredAndOptionalConstPropertiesDataProvider(): array
+    {
+        return $this->combineDataProvider(
+            $this->implicitNullDataProvider(),
+            [
+                ['blue', 'green', 'Invalid value for requiredProperty declined by const constraint'],
+                ['blue', null, 'Invalid value for requiredProperty declined by const constraint'],
+                ['red', 'blue', 'Invalid value for optionalProperty declined by const constraint'],
+                ['red', '0', 'Invalid value for optionalProperty declined by const constraint'],
+                ['red', '', 'Invalid value for optionalProperty declined by const constraint'],
+            ],
+        );
+    }
+
+    /**
+     * @throws FileSystemException
+     * @throws RenderException
+     * @throws SchemaException
+     */
+    public function testProvidedNullValueConstPropertyIsValid(): void
+    {
+        $className = $this->generateClassFromFile('NullValueConstProperty.json', null, false, false);
+
+        $object = new $className(['nullProperty' => null]);
+
+        $this->assertNull($object->getNullProperty());
     }
 }
